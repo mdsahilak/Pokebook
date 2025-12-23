e2d00d16c09f265f00a3501e34cad12f04eec6a1
mdsahilak <mdsahilak.dev@gmail.com>
2025-12-23 14:52:02 +0530

    create detail view

---
+++ Pokebook/PokeDetail/PokemonDetailView.swift
@@ -0,0 +1,141 @@
+//
+//  PokemonDetailView.swift
+//  Pokedex
+//
+//  Created by Sahil Ak on 03/07/2024.
+//
+
+import SwiftUI
+
+/// View to display detailed information about a pokemon
+struct PokemonDetailView: View {
+    @Environment(\.dismiss) var dismiss
+    @StateObject var vm: PokemonDetailViewModel
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView(.vertical) {
+                VStack {
+                    if let pokemon = vm.pokemonInfo {
+                        indicatorView
+                            .accessibilityHidden(true)
+                        
+                        imageCarousel(for: pokemon)
+                            .accessibilityHidden(true)
+                        
+                        heightAndWeightBar(for: pokemon)
+                        
+                        statsView(for: pokemon)
+                    } else {
+                        CircularLoaderView()
+                            .padding(.top, 190)
+                    }
+                }
+                .padding()
+                .task {
+                    await vm.loadPokemonInformation()
+                }
+            }
+            .scrollIndicators(.hidden)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("\(vm.pokemonLink.name.capitalized)")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    dismissButton
+                }
+            }
+        }
+    }
+    
+    /// Image carousel with special zoom effects
+    private func imageCarousel(for pokemon: PokemonInformation) -> some View {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 0) {
+                pokemonImageCard(for: pokemon.imageURL)
+                    .id(0)
+                
+                pokemonImageCard(for: pokemon.altImageURL)
+                    .id(1)
+            }
+            .scrollTargetLayout()
+        }
+        .scrollPosition(id: $vm.currentImageIndex)
+        .scrollIndicators(.hidden)
+        .scrollTargetBehavior(.paging)
+    }
+    
+    /// Height and weight display bar
+    @ViewBuilder
+    private func heightAndWeightBar(for pokemon: PokemonInformation) -> some View {
+        Divider()
+        HStack {
+            Text("Height: \(pokemon.height)")
+            Divider()
+            Text("Weight: \(pokemon.weight)")
+        }
+        Divider()
+    }
+    
+    /// View to display the statics using progress bars
+    @ViewBuilder
+    private func statsView(for pokemon: PokemonInformation) -> some View {
+        ForEach(pokemon.statInfos) { statInfo in
+            ProgressView(value: statInfo.baseStat, total: Constants.statsMaxValue) {
+                HStack {
+                    Text(statInfo.statLink.name.capitalized)
+                    Spacer()
+                    Text("\(statInfo.baseStat, specifier: "%.0f")")
+                }
+            }
+            .padding(7)
+        }
+    }
+    
+    /// Card to display a single sprite of the pokemon
+    private func pokemonImageCard(for url: URL?) -> some View {
+        PokemonImageView(url: url)
+            .frame(maxHeight: 350)
+            .padding()
+            .cardBackground()
+            .padding(7)
+            .containerRelativeFrame(.horizontal)
+            .scrollTransition(.animated, axis: .horizontal) { content, phase in
+                content
+                    .opacity(phase.isIdentity ? 1.0 : 0.7)
+                    .scaleEffect(phase.isIdentity ? 1.0 : 0.7)
+            }
+    }
+    
+    /// Interactive Indicator for the image carousel
+    private var indicatorView: some View {
+        HStack {
+            ForEach(0..<3, id: \.self) { val in
+                let current = vm.currentImageIndex ?? 0
+                
+                Button(action: {
+                    withAnimation {
+                        vm.currentImageIndex = val
+                    }
+                }, label: {
+                    Image(systemName: "circle.fill")
+                        .font(.system(size: 8))
+                        .foregroundStyle(current == val ? Color(.darkGray) : Color(.lightGray))
+                })
+            }
+        }
+        .padding(.bottom, 7)
+    }
+    
+    /// Dismiss button for the sheet's navigation bar
+    private var dismissButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            Label("Dismiss", systemImage: "chevron.down")
+        }
+    }
+}
+
+#Preview {
+    PokemonDetailView(vm: .init(pokemonLink: .mock(), service: MockPokemonService()))
+}
---
+++ Pokebook/PokeDetail/PokemonDetailViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  PokemonDetailViewModel.swift
+//  Pokedex
+//
+//  Created by Sahil Ak on 03/07/2024.
+//
+
+import Foundation
+import Combine
+
+@MainActor
+final class PokemonDetailViewModel: ObservableObject {
+    let pokemonLink: PokemonLink
+    let service: PokemonService
+    
+    /// The detailed information related to the currently selected pokemon
+    @Published var pokemonInfo: PokemonInformation? = nil
+    
+    /// Value indicating the current image shown in the carousel
+    @Published var currentImageIndex: Int? = nil
+    
+    init(pokemonLink: PokemonLink, service: PokemonService) {
+        self.pokemonLink = pokemonLink
+        self.service = service
+    }
+    
+}
+
+// MARK: - API Call Handlers -
+extension PokemonDetailViewModel {
+    /// Fetch detailed information about the selected pokemon
+    public func loadPokemonInformation() async {
+        do {
+            pokemonInfo = try await service.fetchPokemonInformation(for: pokemonLink.id)
+        } catch {
+            print(error)
+        }
+    }
+}
